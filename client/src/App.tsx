ceab31db3bdb01804466fcb24b0aa01fd5a34a8e
noelamorcillojr-create <noelamorcillojr@gmail.com>
2026-08-14 06:47:31 -0400

    Fix routing on GitHub Pages: make wouter aware of the repository base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,23 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
+// Strips the trailing slash so wouter matches both "/base/" and "/base"
+const ROUTER_BASE = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={ROUTER_BASE}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/404" component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
